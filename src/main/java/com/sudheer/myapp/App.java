09f024abc3667bb976f1bd1c8782923c61257a67
sudheer <ysudheerawsdevops@gmail.com>
2022-02-17 09:53:02 +0000

    Project test Commit

---
+++ src/main/java/com/sudheer/myapp/App.java
@@ -0,0 +1,13 @@
+package com.sudheer.myapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
